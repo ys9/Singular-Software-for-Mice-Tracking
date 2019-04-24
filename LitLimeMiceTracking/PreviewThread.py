c0806b2aee31b7ea98ee85233dffff12b333af81
Geoffrey Knox <geoffreyknox56@gmail.com>
2019-04-24 04:51:25 -0600

    added available devices to choose from

--- LitLimeMiceTracking/PreviewThread.py
+++ LitLimeMiceTracking/PreviewThread.py
@@ -25,25 +25,12 @@ class PreviewThread(QThread):
             if self.shouldPause:
                 time.sleep(1)
                 continue
-            #chk1 = time.time()
             ret, frame = self.capture_device.read()
-            #chk2 = time.time()
             if ret:
                 rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 mouse_frame = self.mice_tracker.check_frame(rgbImage)
-                #p = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
-                #convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
                 convertToQtFormat = QImage(mouse_frame.data, mouse_frame.shape[1], mouse_frame.shape[0], QImage.Format_RGB888)
+                #convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
                 p = convertToQtFormat.scaled(self.frame_width/self.division, self.frame_height/self.division, Qt.KeepAspectRatio)
                 self.changePixmap.emit(p)
-            #else:
-            #    break
-            #if chk2 == chk1 or (chk2 - chk1) < 0.01: #don't evaluate if more than 100fps, getting weird values when not doing this
-            #    continue
 
-            #self.frames_counted += int(1/(chk2 - chk1))
-            #self.average_fps = int(self.frames_counted / self.frame_count)
-
-            #self.measured_fps = int(1/(chk2 - chk1))
-            #print("fps = ", self.measured_fps)
-            #print("fps = ", self.average_fps)
